79c6a789b73ca565696bc9d26b52ac90c2502262
Ada Šiurna <38506287+adasiurna@users.noreply.github.com>
2019-02-06 17:41:02 +0200

    added reactstrap and router

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import './App.css';
-import HelloMessage from './HelloMessage';
+
+import NavBar from './components/NavBar';
+import Books from './containers/Books';
+import Authors from './containers/Authors';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <HelloMessage name="Jonai" />
-          <HelloMessage fontWeight="bold" color="yellow" name="Petrai" />
-          <HelloMessage name="Anele" />
-          <HelloMessage name="Monika" />
-          <HelloMessage name="Lukuti" />
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <NavBar />
+
+          <Route exact path="/" component={Books} />
+          <Route path="/books" component={Books} />
+          <Route path="/authors" component={Authors} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap';
+
+import { Link } from 'react-router-dom';
+
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+
+    this.state = {
+      isOpen: false,
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">BTA Books</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <Link to="/books">
+                  <NavLink>Books</NavLink>
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/authors">
+                  <NavLink>Authors</NavLink>
+                </Link>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Add new book</DropdownItem>
+                  <DropdownItem>Add new author</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
